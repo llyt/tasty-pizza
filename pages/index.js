ff989cca07db9a4065f3c52417fa1bd5a7fce2db
llyt <anton@lytkin.me>
2020-06-15 14:33:23 +0300

    add favicon

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const HomePage = () => {
+  return (
+    <>
+      <Head>
+        <title>Tasty Pizza Delivery</title>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+      </Head>
+      <h1>Hello Pizza</h1>
+    </>
+  )
+}
+
+export default HomePage
