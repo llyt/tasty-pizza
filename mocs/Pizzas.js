fd6a9e1b77e36feabc5b0a089c3be177c3b28daf
llyt <anton@lytkin.me>
2020-06-18 13:47:08 +0300

    Added new pizzas

--- mocs/Pizzas.js
+++ mocs/Pizzas.js
@@ -37,110 +37,65 @@ export default [
   },
   {
     image: {
-      src: 'cheese.jpg',
-      alt: 'Cheese Pizza'
+      src: 'cheesy-chicken.jpg',
+      alt: 'Cheesy Chicken Pizza'
     },
-    title: 'Cheese',
-    description: 'Large portion of mozzarella, tomato sauce',
-    startPrice: 4
+    title: 'Cheesy Chicken',
+    description: 'Chicken, cheese sauce, mozzarella, tomatoes',
+    startPrice: 3
   },
   {
     image: {
-      src: 'cheese-with-ham.jpg',
-      alt: 'Cheese with ham Pizza'
+      src: 'hum-and-mushrooms.jpg',
+      alt: 'Hum and Mushrooms Pizza'
     },
-    title: 'Cheese with ham',
-    description: 'Ham, large portion of mozzarella, cream sauce',
-    startPrice: 4
-  },
-  {
-    image: {
-      src: 'pepperoni.jpg',
-      alt: 'Pepperoni Pizza'
-    },
-    title: 'Pepperoni',
-    description: 'Spicy pepperoni, large portion of mozzarella, tomato sauce',
+    title: 'Hum and Mushrooms',
+    description: 'Ham, champignons, large portion of mozzarella, tomato sauce',
     startPrice: 5
   },
   {
     image: {
-      src: 'hawaiian.jpg',
-      alt: 'Hawaiian Pizza'
+      src: 'margherita.jpg',
+      alt: 'Margherita Pizza'
     },
-    title: 'Hawaiian',
-    description: 'Chicken, tomato sauce, mozzarella, pineapple',
+    title: 'Margherita',
+    description: 'Extra portion of mozzarella, tomatoes, Italian herbs, tomato sauce',
     startPrice: 6
   },
   {
     image: {
-      src: 'cheese.jpg',
-      alt: 'Cheese Pizza'
+      src: 'quattro-cheese.jpg',
+      alt: 'Quattro Cheese Pizza'
     },
-    title: 'Cheese',
-    description: 'Large portion of mozzarella, tomato sauce',
-    startPrice: 4
+    title: 'Quattro Cheese',
+    description: 'Extra portion of mozzarella, mix of cheddar and parmesan cheese, blue cheese, cream sauce',
+    startPrice: 7
   },
   {
     image: {
-      src: 'cheese-with-ham.jpg',
-      alt: 'Cheese with ham Pizza'
+      src: 'double-pepperoni.jpg',
+      alt: 'Double Pepperoni Pizza'
     },
-    title: 'Cheese with ham',
-    description: 'Ham, large portion of mozzarella, cream sauce',
-    startPrice: 4
+    title: 'Double Pepperoni',
+    description: 'More spicy pepperoni, larger portion of mozzarella, tomato sauce',
+    startPrice: 8
   },
   {
     image: {
-      src: 'pepperoni.jpg',
-      alt: 'Pepperoni Pizza'
+      src: 'super-meat.jpg',
+      alt: 'Super Meat Pizza'
     },
-    title: 'Pepperoni',
-    description: 'Spicy pepperoni, large portion of mozzarella, tomato sauce',
-    startPrice: 5
+    title: 'Super Meat',
+    description: 'Chicken, beef mitballs, spicy pepperoni, tomato sauce, spicy chorizo, mozzarella, bacon',
+    startPrice: 10
   },
   {
     image: {
-      src: 'hawaiian.jpg',
-      alt: 'Hawaiian Pizza'
+      src: 'four-seasons.jpg',
+      alt: 'Four Seasons Pizza'
     },
-    title: 'Hawaiian',
-    description: 'Chicken, tomato sauce, mozzarella, pineapple',
-    startPrice: 6
-  },
-  {
-    image: {
-      src: 'cheese.jpg',
-      alt: 'Cheese Pizza'
-    },
-    title: 'Cheese',
-    description: 'Large portion of mozzarella, tomato sauce',
-    startPrice: 4
-  },
-  {
-    image: {
-      src: 'cheese-with-ham.jpg',
-      alt: 'Cheese with ham Pizza'
-    },
-    title: 'Cheese with ham',
-    description: 'Ham, large portion of mozzarella, cream sauce',
-    startPrice: 4
-  },
-  {
-    image: {
-      src: 'pepperoni.jpg',
-      alt: 'Pepperoni Pizza'
-    },
-    title: 'Pepperoni',
-    description: 'Spicy pepperoni, large portion of mozzarella, tomato sauce',
-    startPrice: 5
-  },
-  {
-    image: {
-      src: 'hawaiian.jpg',
-      alt: 'Hawaiian Pizza'
-    },
-    title: 'Hawaiian',
-    description: 'Chicken, tomato sauce, mozzarella, pineapple',
-    startPrice: 6
+    title: 'Four Seasons',
+    description: 'Enlarged portion of mozzarella, ham, spicy pepperoni, cheese cubes, tomatoes, champignons, tomato sauce, Italian herbs',
+    startPrice: 9
   }
 ]
